747cb72650868a1ae7654526beea11c8385bc004
romanmuni8 <romanmuni8@gmail.com>
2023-06-09 10:39:21 -0400

    Initial Commit

---
+++ VentureOut/VentureOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentureOutApp.swift
+//  VentureOut
+//
+//  Created by Roman on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct VentureOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
